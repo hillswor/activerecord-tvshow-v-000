93918941254daafb87b2c33353ae9ab6644c4ce4
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-26 02:20:04 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    Show.maximum(:rating)
+    Show.maximum("rating")
   end
 
   def self.most_popular_show
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-    Show.minimum(:rating)
+    Show.minimum("rating")
   end
 
   def self.least_popular_show
@@ -17,7 +17,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.ratings_sum
-    Show.sum(:rating)
+    Show.sum("rating")
   end
 
   def self.popular_shows
